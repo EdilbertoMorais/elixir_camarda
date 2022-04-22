f01655390d886652910d9e9bb960ba12e9734c0f
EdilbertoMorais <edilbertocmorais@gmail.com>
2022-04-22 18:04:03 -0300

    corrigindo  linha 25 modulo jogo

--- ex_mon/lib/jogo.ex
+++ ex_mon/lib/jogo.ex
@@ -22,7 +22,7 @@ defmodule ExMon.Jogo do
   end
 
   def jogador, do: Map.get(info(), :jogador)
-  ExM
+
   def buscar_jogador(jogador), do: Map.get(info(), jogador)
 
   defp atualizar_estado_jogo(
